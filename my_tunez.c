67f3e2e43864b3d4b9a976977b9a76c42539bd97
BrianLim167 <blim167@gmail.com>
2017-10-16 15:32:19 -0400

    began basic song adding

---
+++ my_tunez.c
@@ -0,0 +1,15 @@
+void add_to_table(struct song_node * table,
+			   struct song_node * song){
+  insert( table[ song->name[0] - 'a' ], song );
+}
+
+struct node * insert;
+
+  return NULL;
+}
+
+int main(){
+  struct song_node * table[26];
+
+  return 0;
+}
