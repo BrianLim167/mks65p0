bb963b9c9484f2c5d38bf6a97009f4982630dc4d
BrianLim167 <blim167@gmail.com>
2017-10-17 15:32:14 -0400

    wrote song creation, printing

--- my_tunez.c
+++ my_tunez.c
@@ -1,15 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "my_tunez.h"
+
 void add_to_table(struct song_node * table,
-			   struct song_node * song){
-  insert( table[ song->name[0] - 'a' ], song );
+		  struct song_node * song){
+  //insert_front( table[ (song->name)[0] - 'a' ], song );
 }
 
-struct node * insert;
+struct song_node * new_song(char * name, char * artist){
+  struct song_node * new = (struct song_node *) malloc( sizeof(struct song_node) );
+  strcpy(new->name, name);
+  strcpy(new->artist, artist);
+  return new;
+}
 
-  return NULL;
+struct song_node * insert_front(struct song_node * list,
+				struct song_node * song){
+  song->next = list;
+  return song;
+}
+
+void song_to_string(struct song_node * song){
+  chr ans[] =;
+  song->name song->artist;
 }
 
 int main(){
   struct song_node * table[26];
 
+  print_song(new_song("vfbn","auietyvgie"));
+  
   return 0;
 }
--- my_tunez.h
+++ my_tunez.h
@@ -4,3 +4,14 @@ struct song_node{
   struct song_node *next;
 };
 
+void add_to_table(struct song_node * table,
+		  struct song_node * song);
+
+struct song_node * new_song(char * name, char * artist);
+
+struct song_node * insert_front(struct song_node * list,
+				struct song_node * song);
+
+void print_song(struct song_node * song);
+
+int main();
