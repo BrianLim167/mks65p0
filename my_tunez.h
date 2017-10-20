dbc442d6b6e4f0aa6cc93225223e0dcb7cecbc94
BrianLim167 <blim167@gmail.com>
2017-10-20 01:23:30 -0400

    updated headers

--- my_tunez.h
+++ my_tunez.h
@@ -13,17 +13,21 @@ struct song_node * new_song(char * name, char * artist);
 
 struct song_node * insert_front(struct song_node * list, char * name, char * artist);
 
-void print_song(struct song_node * song);
-
-void print_list(struct song_node * start);
-
 struct song_node * insert_in_order(struct song_node * list, char * name, char * artist);
 
 struct song_node * find_song_with_name(struct song_node * list, char * name, char * artist);
 
-int main();
 
 struct song_node * find_song(struct song_node * list, char * name, char * artist);
 struct song_node * find_artist(struct song_node * list, char * artist);
 
+int length(struct song_node * list);
+struct song_node * random_song(struct song_node * list);
+
 struct song_node * free_list(struct song_node * node);
+
+void print_song(struct song_node * song);
+
+void print_list(struct song_node * start);
+
+int main();
